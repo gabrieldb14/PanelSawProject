39f3a15788b44b6a422f73336fcd943c4386cac4
Gabriel Borges <gabrieldborges14@outlook.com>
2025-12-26 11:14:25 -0300

    Release version 0.5

---
+++ HMI_PanelSaw/Model/PusherClampState.cs
@@ -0,0 +1,12 @@
+﻿namespace HMI_PanelSaw.Models
+{
+    public enum PusherClampState : short
+    {
+        Idle = 0,
+        Positioning = 1,
+        Clamping = 2,
+        Clamped = 3,
+        Unclamping = 4,
+        Error = 5
+    }
+}
